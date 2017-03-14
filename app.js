5ef9528c38f47693c7765cca578b93981c8e55e3
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-14 00:08:31 -0300

    serving Json data

--- app.js
+++ app.js
@@ -108,13 +108,30 @@
 
 //--------------------------------------------------------------------------------------------------------
 //serving html pages to the user
-   var http=require('http');
+// var http=require('http');
+// var fs=require('fs');
+// var server=http.createServer(function(req,res){            //creating a server using http module then the func gets trigger which takes 2 arguments the request and the response
+//      console.log('request was made '+ req.url);
+//       res.writeHead(200,{'Content-Type':'text/html'});    //sending the response contains 2 things- status and Content
+//       var myReadStream=fs.createReadStream(__dirname+'/index.html','utf8');
+//       myReadStream.pipe(res);
+// });
+
+// server.listen(3000,'127.0.0.1');
+// console.log('listening to port 3000');
+
+//--------------------------------------------------------------------------------------------------------
+//serving JSON data
+var http=require('http');
 var fs=require('fs');
 var server=http.createServer(function(req,res){            //creating a server using http module then the func gets trigger which takes 2 arguments the request and the response
      console.log('request was made '+ req.url);
-      res.writeHead(200,{'Content-Type':'text/html'});    //sending the response contains 2 things- status and Content
-      var myReadStream=fs.createReadStream(__dirname+'/index.html','utf8');
-      myReadStream.pipe(res);
+      res.writeHead(200,{'Content-Type':'application/json'});    //sending the response contains 2 things- status and Content
+      var myObj={
+          name:'rishabh',
+          age:18
+        }
+        res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000,'127.0.0.1');
