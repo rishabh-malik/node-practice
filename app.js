616e92dea00a59c156818fa297e5e28e3d2ed8c5
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-14 00:01:20 -0300

    serving html pages to user

--- app.js
+++ app.js
@@ -94,17 +94,47 @@
 
 //---------------------------------------------------------------------------------------------------------
 //giving output in streams
-var http=require('http');
+// var http=require('http');
+// var fs=require('fs');
+// var server=http.createServer(function(req,res){            //creating a server using http module then the func gets trigger which takes 2 arguments the request and the response
+//      console.log('request was made '+ req.url);
+//       res.writeHead(200,{'Content-Type':'text/plain'});    //sending the response contains 2 things- status and Content
+//       var myReadStream=fs.createReadStream(__dirname+'/read.txt','utf8');
+//       myReadStream.pipe(res);
+// });
+
+// server.listen(3000,'127.0.0.1');
+// console.log('listening to port 3000');
+
+//--------------------------------------------------------------------------------------------------------
+//serving html pages to the user
+   var http=require('http');
 var fs=require('fs');
 var server=http.createServer(function(req,res){            //creating a server using http module then the func gets trigger which takes 2 arguments the request and the response
      console.log('request was made '+ req.url);
-      res.writeHead(200,{'Content-Type':'text/plain'});    //sending the response contains 2 things- status and Content
-      var myReadStream=fs.createReadStream(__dirname+'/read.txt','utf8');
+      res.writeHead(200,{'Content-Type':'text/html'});    //sending the response contains 2 things- status and Content
+      var myReadStream=fs.createReadStream(__dirname+'/index.html','utf8');
       myReadStream.pipe(res);
 });
 
 server.listen(3000,'127.0.0.1');
 console.log('listening to port 3000');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
